63c4c33af570bbeb8391cd1aa943182faf36852e
Stephen Urena <surena@Stephens-MacBook-Air.local>
2017-03-14 20:35:16 -0700

    modified files

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -104,7 +104,9 @@ function initMap() {
         }, function(results, status) { //this anonymus function is called back when the results and status are received from geocode function 
             if (status == google.maps.GeocoderStatus.OK) {
                 userLatLng = results[0].geometry.location; // Assign the latitude and longtude object from the first result to userLatLng variable
+              	localWWAPI(userLatLng);
               	worldWideWeather(userLatLng);
+
                    console.log(userLatLng.lat())
             }
 
@@ -209,19 +211,45 @@ function geolocate() {
 //====================================================================================
 //---------------------------world wide weather online--------------------------------
 //--------------------------------Local-----------------------------------------------
-// var localAPIKey = "094f52c21f8d4c3dbff24712170903";
-// var url = "https://api.worldweatheronline.com/premium/v1/weather.ashx?";
-// 	url += "&q=" + userSearch;
-//     url += "&format=json";
-//     url += "&key="+ localAPIKey;
-//     url += "&includelocation=yes";
-//     url += "&fx=" + "yes";
-// 	$.ajax({
-// 	      url: url,
-// 	      method: 'GET',
-// 	    }).done(function(data) {
-// 	    	console.log(data)
-// 	    });
+function localWWAPI (userLatLng){
+weatherLatlng = userLatLng.toString();
+       weatherLatlng = weatherLatlng.substring(1, weatherLatlng.length -1);
+       console.log(weatherLatlng);
+var apiKey = "094f52c21f8d4c3dbff24712170903";
+var url = "https://api.worldweatheronline.com/premium/v1/weather.ashx?";
+           url += "&q=" + weatherLatlng;
+           url += "&format=json";
+           url += "&key="+ apiKey;
+           url += "&includelocation=yes";
+           url += "&fx=" + "yes";
+
+       $.ajax({
+         // dataType: "jsonp",
+         url: url,
+         method: 'GET',
+         // success: searchCallback
+       }).done(function(dataSnap) {
+
+           var serverReq = dataSnap.data;
+           console.log(serverReq)
+
+    		var currentTemp = serverReq.current_condition[0].temp_F;
+    		var weatherIcon = serverReq.current_condition[0].weatherIconUrl[0].value;
+    		var feelsLike = serverReq.current_condition[0].FeelsLikeF;
+    		var humidity = serverReq.current_condition[0].humidity;
+    		var windSpeed = serverReq.current_condition[0].windspeedMiles;
+
+    		console.log(weatherIcon);
+    		
+    		console.log(currentTemp);
+
+           $("#weatherIcon").attr("src", weatherIcon).css("border-radius", "100%");
+           $(".tempF").html(currentTemp + " &deg;F");
+           $(".feelsLike").html("Feels like " + feelsLike + " &deg;F");
+           $(".humidity").html("Humidity " + humidity);
+           $(".windSpeed").html("Wind speed " + windSpeed);
+});
+}
 //---------------------------end of local----------------------------------------------
 
 //***********************Global Variables for World Weather-Marine***************************
@@ -242,12 +270,56 @@ var hourlySwellPer;
 var hourlyTime;
 //arrays to hold data for graph
 var weeklyForecast = []; //to make it an array of arrays [][]
+
+var date;
+var arrDate = [];
+
+var date1;
+var arrDate1 = [];
+
+var date2;
+var arrDate2 = [];
+
+var date3;
+var arrDate3 = [];
+
+var date4;
+var arrDate4 = [];
+
 var arrForecast = [];
 var arrWaterTemp = [];
 var arrSwell = [];
 var arrSwellDir = [];
 var arrSwellPer = [];
 var arrTime = [];
+
+var arrForecast2 = [];
+var arrWaterTemp2 = [];
+var arrSwell2 = [];
+var arrSwellDir2 = [];
+var arrSwellPer2 = [];
+var arrTime2 = [];
+
+var arrForecast3 = [];
+var arrWaterTemp3 = [];
+var arrSwell3 = [];
+var arrSwellDir3 = [];
+var arrSwellPer3 = [];
+var arrTime3 = [];
+
+var arrForecast4 = [];
+var arrWaterTemp4 = [];
+var arrSwell4 = [];
+var arrSwellDir4 = [];
+var arrSwellPer4 = [];
+var arrTime4 = [];
+
+var arrForecast5 = [];
+var arrWaterTemp5 = [];
+var arrSwell5 = [];
+var arrSwellDir5 = [];
+var arrSwellPer5 = [];
+var arrTime5 = [];
 //current tide info
 var tideType;
 var tideTime;
@@ -293,19 +365,8 @@ weatherLatlng = userLatLng.toString();
 	    	iconURL = serverReq.weather[0].hourly["0"].weatherIconUrl["0"].value;
 	    	console.log(iconURL)
 
-	    	$("#weatherIcon").attr("src", iconURL);
-
-	    	maxTempF = weather.maxtempF;
-
-	    	tempF = weather.hourly["0"].tempF;
-	    	console.log(tempF);
-
-	    	currentTime = moment();
-	    	console.log("Current Time is: " + currentTime);
-
-	    	$(".tempF").html( maxTempF + " &deg;F");
-
-			$("#timeInfoHere").empty();
+			$("#astronomyInfo").empty();
+			$("#tideInfo").empty();
 
 			//adds current astronomy data
 			var astronomy = weather.astronomy[0];
@@ -313,10 +374,11 @@ weatherLatlng = userLatLng.toString();
 				var moonset = astronomy.moonrise;
 				var sunrise = astronomy.sunrise;
 				var sunset = astronomy.sunset;
-			$("#astronomyInfo").html("<tr><th>" + "Sunrise" + "</th><td>" + sunrise + "</td></tr>")
-							   .html("<tr><th>" + "Sunset" + "</th><td>" + sunset + "</td></tr>")
-							   .html("<tr><th>" + "Moonrise" + "</th><td>" + moonrise + "</td></tr>")
-							   .html("<tr><th>" + "Moonset" + "</th><td>" + moonset + "</td></tr>");
+			$("#astronomyInfo").append("<tr><th>" + "Sunrise" + "</th><td>" + sunrise + "</td></tr>") 
+							   .append("<tr><th>" + "Sunset" + "</th><td>" + sunset + "</td></tr>")
+							   .append("<tr><th>" + "Moonrise" + "</th><td>" + moonrise + "</td></tr>")
+							   .append("<tr><th>" + "Moonset" + "</th><td>" + moonset + "</td></tr>");
+							
 
 			//add current tide info
 			var tideInfo = weather.tides[0];
@@ -325,36 +387,26 @@ weatherLatlng = userLatLng.toString();
 				tideType = tideInfo.tide_data[i].tide_type;
 				tideTime = tideInfo.tide_data[i].tideTime;
 				tideHeight = tideInfo.tide_data[i].tideHeight_mt;
-				$("#tideInfo").html("<tr><th>" + tideType + "</th><td>" + tideTime + "</td><td>" + tideHeight + " ft" + "</td></tr>")
+				$("#tideInfo").append("<tr><th>" + tideType + "</th><td>" + tideTime + "</td><td>" + tideHeight + " ft" + "</td></tr>");
 
 
 			};
 
-
-	    	// for (y = 0; y < 7; y++) {
-	    	// 	weeklyForecast = serverReq.weather[y];
-	    	// 	console.log(weeklyForecast);
-	    	// 		arrForecast.push(weeklyForecast);
-	    		// hourlyTempF = serverReq.weather[y].hourly[i].tempF;
-	    		// // console.log(hourlyTempF, ",");
-	    		// iconURL = serverReq.weather[y].hourly[i].weatherIconUrl[0].value
-	    		// 	//maybe set an arr for the icons to use for graph come back to it
-	    		// hourlySwell = serverReq.weather[y].hourly[i].swellHeight_ft;
-	    		// 	arrSwell.push(hourlySwell);
-	    		// hourlySwellDir = serverReq.weather[0].hourly[i].swellDir16Point;
-	    		// 	arrSwellDir.push(hourlySwellDir);
-	    		// hourlySwellPer = serverReq.weather[0].hourly[i].swellPeriod_secs;
-	    		// 	arrSwellPer.push(hourlySwellPer);
-	    		// hourlyWaterTemp = serverReq.weather[0].hourly[i].waterTemp_F;
-	    		// 	arrWaterTemp.push(hourlyWaterTemp);
-	    		// hourlyTime = serverReq.weather[0].hourly[i].time;
-	    		// 	arrTime.push(hourlyTime);
-
+			//dates for charts.js
+	    		date = serverReq.weather[0].date;
+	    			arrDate.push(moment(date).format("dd M/D"));
+	    		date2 = serverReq.weather[1].date;
+	    			arrDate1.push(moment(date2).format("dd M/D"));
+	    		date3 = serverReq.weather[2].date;
+	    			arrDate2.push(moment(date3).format("dd M/D"));
+	    		date4 = serverReq.weather[3].date;
+	    			arrDate3.push(moment(date4).format("dd M/D"));
+	    		date5 = serverReq.weather[4].date;
+	    			arrDate4.push(moment(date5).format("dd M/D"));
+	    		console.log(date);
 		    	for (i = 0; i < 4; i++) {
+		
 		    		hourlyTempF = serverReq.weather[0].hourly[i].tempF;
-		    		// console.log(hourlyTempF, ",");
-		    		// iconURL = serverReq.weather[0].weatherIconUrl[i].value;
-		    			//maybe set an arr for the icons to use for graph come back to it
 		    		hourlySwell = serverReq.weather[0].hourly[i].swellHeight_ft;
 		    			arrSwell.push(hourlySwell);
 		    		hourlySwellDir = serverReq.weather[0].hourly[i].swellDir16Point;
@@ -365,15 +417,63 @@ weatherLatlng = userLatLng.toString();
 		    			arrWaterTemp.push(hourlyWaterTemp);
 		    		hourlyTime = serverReq.weather[0].hourly[i].time;
 		    			arrTime.push(hourlyTime);
-		    	// 	// var hourlyTime = moment(serverReq.weather[i].hourly[0].time).format("hh:mm A");
+		    	};
+		    	for (i = 0; i < 4; i++) {
+		    		hourlyTempF = serverReq.weather[1].hourly[i].tempF;
 
-		    	// 	// console.log("Time: " + hourlyTime);
+		    		hourlySwell = serverReq.weather[1].hourly[i].swellHeight_ft;
+		    			arrSwell2.push(hourlySwell);
+		    		hourlySwellDir = serverReq.weather[1].hourly[i].swellDir16Point;
+		    			arrSwellDir2.push(hourlySwellDir);
+		    		hourlySwellPer = serverReq.weather[1].hourly[i].swellPeriod_secs;
+		    			arrSwellPer2.push(hourlySwellPer);
+		    		hourlyWaterTemp = serverReq.weather[1].hourly[i].waterTemp_F;
+		    			arrWaterTemp2.push(hourlyWaterTemp);
+		    		hourlyTime = serverReq.weather[1].hourly[i].time;
+		    			arrTime2.push(hourlyTime);
+		    	};
+		    	for (i = 0; i < 4; i++) {
+		    		hourlyTempF = serverReq.weather[2].hourly[i].tempF;
+		    	
+		    		hourlySwell = serverReq.weather[2].hourly[i].swellHeight_ft;
+		    			arrSwell3.push(hourlySwell);
+		    		hourlySwellDir = serverReq.weather[2].hourly[i].swellDir16Point;
+		    			arrSwellDir3.push(hourlySwellDir);
+		    		hourlySwellPer = serverReq.weather[2].hourly[i].swellPeriod_secs;
+		    			arrSwellPer3.push(hourlySwellPer);
+		    		hourlyWaterTemp = serverReq.weather[2].hourly[i].waterTemp_F;
+		    			arrWaterTemp3.push(hourlyWaterTemp);
+		    		hourlyTime = serverReq.weather[2].hourly[i].time;
+		    			arrTime3.push(hourlyTime);
+		    	};
+		    	for (i = 0; i < 4; i++) {
+		    		hourlyTempF = serverReq.weather[3].hourly[i].tempF;
 
-		    	// 	// $("#timeInfoHere").append("<tr><td>" + hourlyTime + "</td><td>" + "<img src='" + iconURL + "' class='img-responsive d-inline weatherIcon'> " + hourlyTempF + " &deg;F" + "</td><td>" + hourlySwell + " ft." + "</td><td>" + hourlySwellDir + "</td><td>" + hourlySwellPer + " secs" + "</td><td>" + hourlyWaterTemp + " &deg;F" + "</td></tr>");
-
-		    		
-		    	// };
-		    };
+		    		hourlySwell = serverReq.weather[3].hourly[i].swellHeight_ft;
+		    			arrSwell4.push(hourlySwell);
+		    		hourlySwellDir = serverReq.weather[3].hourly[i].swellDir16Point;
+		    			arrSwellDir4.push(hourlySwellDir);
+		    		hourlySwellPer = serverReq.weather[3].hourly[i].swellPeriod_secs;
+		    			arrSwellPer4.push(hourlySwellPer);
+		    		hourlyWaterTemp = serverReq.weather[3].hourly[i].waterTemp_F;
+		    			arrWaterTemp4.push(hourlyWaterTemp);
+		    		hourlyTime = serverReq.weather[3].hourly[i].time;
+		    			arrTime4.push(hourlyTime);
+		    	};
+		    	for (i = 0; i < 4; i++) {
+		    		hourlyTempF = serverReq.weather[4].hourly[i].tempF;
+		    
+		    		hourlySwell = serverReq.weather[4].hourly[i].swellHeight_ft;
+		    			arrSwell5.push(hourlySwell);
+		    		hourlySwellDir = serverReq.weather[4].hourly[i].swellDir16Point;
+		    			arrSwellDir5.push(hourlySwellDir);
+		    		hourlySwellPer = serverReq.weather[4].hourly[i].swellPeriod_secs;
+		    			arrSwellPer5.push(hourlySwellPer);
+		    		hourlyWaterTemp = serverReq.weather[4].hourly[i].waterTemp_F;
+		    			arrWaterTemp5.push(hourlyWaterTemp);
+		    		hourlyTime = serverReq.weather[4].hourly[i].time;
+		    			arrTime5.push(hourlyTime);
+		    	};
 
 	    }); 
 	    //arrays for to use data for chart.js
@@ -384,33 +484,96 @@ weatherLatlng = userLatLng.toString();
 	    console.log(arrSwellDir);
 	    console.log(arrSwell);
 
-
 }
 //====================================================================================
 //********************************* Charts.js  ***************************************
-var ctx = $("#myChart");
+var ctx = $("#myChart"); 
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ["6AM", "Noon", "6PM", "12AM"],
         datasets: [{
-            label: 'Max Surf in ft',
+            label: arrDate,
             data: arrSwell,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+            ],
+            borderWidth: 2
+        },
+        {
+            label: arrDate1,
+            data: arrSwell2,
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
                 'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 2
+        },
+        {
+            label: arrDate2,
+            data: arrSwell2,
+            backgroundColor: [
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
                 'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 2
+        },
+        {
+            label: arrDate3,
+            data: arrSwell3,
+            backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+            ],
+            borderColor: [
                 'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(75, 192, 192, 1)',
+            ],
+            borderWidth: 2
+        },
+        {
+            label: arrDate4,
+            data: arrSwell5,
+            backgroundColor: [
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(153, 102, 255, 1)',
             ],
             borderWidth: 2
         }]
@@ -420,10 +583,16 @@ var myChart = new Chart(ctx, {
             yAxes: [{
                 ticks: {
                     beginAtZero:true
-                }
+                },
+                scaleLabel: {
+        			display: true,
+        			labelString: 'Max Surf in ft',
+        		}
             }]
         }
-    }
+    },
+
+    responsive: true,
 });
 //====================================================================================
 
